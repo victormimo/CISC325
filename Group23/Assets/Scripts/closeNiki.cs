2b372a9a56cd8ee75c37f460eec0e2ce32a582f4
kevinkid135 <kevinkid228@hotmail.com>
2017-04-02 20:34:49 -0400

    Portraits work

---
+++ Group23/Assets/Scripts/closeNiki.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeNiki : selectNiki
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Group23/Assets/Scripts/selectNiki.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class selectNiki : MonoBehaviour
+{
+
+    protected static GameObject person;
+    // Use this for initialization
+    void Start()
+    {
+        var _person = GameObject.FindGameObjectWithTag("AvatarNiki");
+        if (!_person)
+        {
+            Debug.LogError("Could not find person");
+        }
+        else
+        {
+            person = _person;
+        }
+
+        // code should be somewhere else:
+        // hide's person on startup
+        hidePerson();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void showPerson()
+    {
+        person.SetActive(true);
+    }
+
+    public void hidePerson()
+    {
+        person.SetActive(false);
+    }
+
+}
