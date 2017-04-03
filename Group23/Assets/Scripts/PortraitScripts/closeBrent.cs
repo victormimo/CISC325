d2d8a84b2b277f201e759b592df67e17f32d148c
kevinkid135 <kevinkid228@hotmail.com>
2017-04-02 21:12:30 -0400

    Added brent's scripts
    Brent avatar works and all avatars are under their respective portraits

--- Group23/Assets/Scripts/PortraitScripts/closeBrent.cs
+++ Group23/Assets/Scripts/PortraitScripts/closeBrent.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class group : MonoBehaviour {
+public class closeBrent : selectBrent {
 
 	// Use this for initialization
 	void Start () {
